16f08eee441de4f196f6e90cc78ceeac3eed53cd
dustinwhisman <whisman.dustin@gmail.com>
2023-02-10 12:30:11 -0700

    feat: set up settings form markup/styles

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.component.html',
-  styleUrls: ['./settings.component.css']
+  styleUrls: ['./settings.component.css'],
 })
 export class SettingsComponent {
+  settingsForm = this.formBuilder.group({
+    timerDuration: '30',
+    letterPositioning: 'linear',
+  });
 
+  constructor(private formBuilder: FormBuilder) {}
+
+  onSubmit(): void {
+    console.log('Your settings have been saved', this.settingsForm.value);
+  }
 }
