b9b337947c0c8b0d8e32910bde8c51005ab85722
dustinwhisman <whisman.dustin@gmail.com>
2023-02-10 11:53:09 -0700

    feat: stub out settings page and link to it

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { GameSelectionComponent } from './game-selection/game-selection.component';
 import { LettersGameComponent } from './letters-game/letters-game.component';
 import { NumbersGameComponent } from './numbers-game/numbers-game.component';
+import { SettingsComponent } from './settings/settings.component';
 
 const routes: Routes = [
   { path: '', component: GameSelectionComponent },
   { path: 'letters-game', component: LettersGameComponent },
   { path: 'numbers-game', component: NumbersGameComponent },
+  { path: 'settings', component: SettingsComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { GameSelectionComponent } from './game-selection/game-selection.component';
 import { LettersGameComponent } from './letters-game/letters-game.component';
 import { NumbersGameComponent } from './numbers-game/numbers-game.component';
+import { SettingsComponent } from './settings/settings.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     GameSelectionComponent,
     LettersGameComponent,
-    NumbersGameComponent
+    NumbersGameComponent,
+    SettingsComponent
   ],
   imports: [
     BrowserModule,
