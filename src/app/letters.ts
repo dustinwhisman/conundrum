f6c23bf987caf791d4c893aaf8b51e6309040635
dustinwhisman <whisman.dustin@gmail.com>
2023-02-10 13:50:38 -0700

    feat: add distribution of vowels and consonants

---
+++ src/app/letters.ts
@@ -0,0 +1,32 @@
+export const vowels = [
+  ...Array(15).fill('a'),
+  ...Array(21).fill('e'),
+  ...Array(13).fill('i'),
+  ...Array(13).fill('o'),
+  ...Array(5).fill('u'),
+];
+
+export const consonants = [
+  ...Array(2).fill('b'),
+  ...Array(3).fill('c'),
+  ...Array(6).fill('d'),
+  ...Array(2).fill('e'),
+  ...Array(3).fill('f'),
+  ...Array(3).fill('g'),
+  ...Array(1).fill('h'),
+  ...Array(1).fill('j'),
+  ...Array(1).fill('k'),
+  ...Array(5).fill('l'),
+  ...Array(4).fill('m'),
+  ...Array(7).fill('n'),
+  ...Array(4).fill('p'),
+  ...Array(1).fill('q'),
+  ...Array(9).fill('r'),
+  ...Array(9).fill('s'),
+  ...Array(9).fill('t'),
+  ...Array(2).fill('v'),
+  ...Array(2).fill('w'),
+  ...Array(1).fill('x'),
+  ...Array(1).fill('y'),
+  ...Array(1).fill('z'),
+];
