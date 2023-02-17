27a6ac329364be5a8ae21caf0ee1a7a577fea45c
dustinwhisman <whisman.dustin@gmail.com>
2023-02-17 09:57:53 -0700

    refactor: use radio-group component in lieu of fieldset legend

--- src/app/radio-group/radio-group.component.ts
+++ src/app/radio-group/radio-group.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-radio-group',
   templateUrl: './radio-group.component.html',
-  styleUrls: ['./radio-group.component.css']
+  styleUrls: ['./radio-group.component.css'],
 })
 export class RadioGroupComponent {
-
+  @Input() legend: string | undefined;
 }
